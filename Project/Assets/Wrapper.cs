6c0eaefb570f405671e817e555c7ee13dee16beb
Jacob Williams <squalljacob@hotmail.co.uk>
2017-04-22 10:55:45 +0100

    Created a basic script to wrap around test chunks left/right

---
+++ Project/Assets/Wrapper.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wrapper : MonoBehaviour
+{
+    public GameObject[] objects;
+    public int centerObject = 2;
+    public int chunkSize = 2;
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		if(Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            MoveRight();
+        }
+        else if(Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            MoveLeft();
+        }
+	}
+
+    private void MoveRight()
+    {
+        int oldChunkIndex = centerObject - 2;
+        if (oldChunkIndex < 0)
+        {
+            oldChunkIndex += objects.Length;
+        }
+        GameObject currentChunk = objects[oldChunkIndex];
+        Vector3 newPos = currentChunk.transform.position;
+        newPos.x += chunkSize * 5;
+        currentChunk.transform.position = newPos;
+        centerObject++;
+        centerObject = centerObject % objects.Length;
+    }
+
+    private void MoveLeft()
+    {
+        int oldChunkIndex = centerObject + 2;
+        oldChunkIndex = oldChunkIndex % objects.Length;
+        GameObject currentChunk = objects[oldChunkIndex];
+        Vector3 newPos = currentChunk.transform.position;
+        newPos.x -= chunkSize * 5;
+        currentChunk.transform.position = newPos;
+        centerObject--;
+        if (centerObject < 0)
+        {
+            centerObject += objects.Length;
+        }
+    }
+}
