562ab57b97458454d28189c679cf5d95b223b08e
Jacob Williams <squalljacob@hotmail.co.uk>
2017-04-22 21:20:23 +0100

    Added a simple shooting mechanic which fires a bullet from a pool

---
+++ Project/Assets/BasicWeapon.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Netaphous.Utilities;
+
+public class BasicWeapon : BasicPoolManager
+{
+    [SerializeField]
+    private Transform bulletSpawn;
+
+    // Use this for initialization
+    void Start ()
+    {
+        CreatePool();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate ()
+    {
+		if(Input.GetMouseButtonDown(0))
+        {
+            Fire();
+        }
+	}
+
+    private void Fire()
+    {
+        GameObject bullet = GetPooledItem();
+
+        if (bullet != null)
+        {
+            bullet.transform.position = bulletSpawn.position;
+            bullet.transform.rotation = bulletSpawn.rotation;
+            bullet.SetActive(true);
+        }
+    }
+}
---
+++ Project/Assets/BulletHit2D.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BulletHit2D : MonoBehaviour
+{
+    // Public variables
+    // To be set in the editor
+    public int damage = 1;
+    public string damageTag = "";
+	[HideInInspector]
+	public int health = 1;
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag (damageTag)) 
+		{
+			other.SendMessage ("TakeDamage", damage);
+
+			print (health);
+			health--;
+			print (health);
+			if (health > 0) 
+			{
+				return;
+			}
+			gameObject.SetActive (false);
+		}
+    }
+}
