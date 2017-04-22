a1e6becf2a4d12b0be03e17d51c99e5c46b489bd
Jacob Williams <squalljacob@hotmail.co.uk>
2017-04-23 00:44:04 +0100

    Added a zombie spawner to the game scene, as well as multiple spawning points

--- Project/Assets/ZombieSpawner.cs
+++ Project/Assets/ZombieSpawner.cs
@@ -5,16 +5,55 @@ using Netaphous.Utilities;
 
 public class ZombieSpawner : BasicPoolManager
 {
+    [SerializeField]
+    private float spawnRate = 2.0f;
+    private Transform[] spawnPoints;
+    // Use this for initialization
+    void Start()
+    {
+        CreatePool();
+        GameObject[] temp = GameObject.FindGameObjectsWithTag("ZombieSpawn");
+        spawnPoints = new Transform[temp.Length];
+        for (int i = 0; i < temp.Length; i++)
+        {
+            spawnPoints[i] = temp[i].transform;
+        }
+        InvokeRepeating("TrySpawnZombie", 5.0f, spawnRate);
+    }
 
-	// Use this for initialization
-	void Start ()
+    // Update is called once per frame
+    void Update()
     {
-		
-	}
-	
-	// Update is called once per frame
-	void Update ()
+
+    }
+
+    private void TrySpawnZombie()
     {
-		
-	}
+        bool validSpawn = false;
+        while (!validSpawn)
+        {
+            int rnd = Random.Range(0, spawnPoints.Length);
+            Vector2 prnt = spawnPoints[rnd].parent.GetComponent<WorldChunk>().GetChunkIndex();
+            Vector2 player = GameObject.FindWithTag("Player").GetComponent<PlayerMovement>().currentChunk;
+            bool validX = (prnt.x > player.x + 1 || prnt.x < (player.x + 1) % 5) &&
+                (prnt.x < player.x - 1 || (prnt.x > player.x && prnt.x < player.x + 4)); 
+            bool validY = (prnt.y > player.y + 1 || prnt.y < (player.y + 1) % 5) &&
+                (prnt.y < player.y - 1 || (prnt.y > player.y && prnt.y < player.y + 4));
+            if (validX || validY)
+            {
+                validSpawn = true;
+                SpawnZombie(spawnPoints[rnd].position);
+            }
+        }
+    }
+    private void SpawnZombie(Vector3 spawnPoint)
+    {
+        GameObject zombie = GetPooledItem();
+
+        if (zombie != null)
+        {
+            zombie.transform.position = spawnPoint;
+            zombie.SetActive(true);
+        }
+    }
 }
