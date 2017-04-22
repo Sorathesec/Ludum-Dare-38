dccba066ee5d446e09944d35a567bcbd0da86ea8
Jacob Williams <squalljacob@hotmail.co.uk>
2017-04-22 11:39:39 +0100

    Revert "Added rows to the game, random colour for chunks and vertical tile wraparound"
    This reverts commit 2c6bd5db7d6035577584ff241c9330840ae21586.

--- Project/Assets/Wrapper.cs
+++ Project/Assets/Wrapper.cs
@@ -4,21 +4,15 @@ using UnityEngine;
 
 public class Wrapper : MonoBehaviour
 {
-    public Row[] rows;
+    public GameObject[] objects;
     public int centerObject = 2;
     public int chunkSize = 2;
-	
-    void Start()
+	// Use this for initialization
+	void Start ()
     {
-        rows = GetComponentsInChildren<Row>();
-        centerObject = rows.Length / 2;
-        if(centerObject == rows.Length / 2.0f)
-        {
-            centerObject--;
-        }
-        Row.chunkSize = chunkSize;
-    }
-
+		
+	}
+	
 	// Update is called once per frame
 	void Update ()
     {
@@ -30,59 +24,35 @@ public class Wrapper : MonoBehaviour
         {
             MoveLeft();
         }
-        else if(Input.GetKeyDown(KeyCode.UpArrow))
-        {
-            MoveUp();
-        }
-        else if(Input.GetKeyDown(KeyCode.DownArrow))
-        {
-            MoveDown();
-        }
 	}
 
     private void MoveRight()
     {
-        foreach(Row row in rows)
+        int oldChunkIndex = centerObject - 2;
+        if (oldChunkIndex < 0)
         {
-            row.MoveRight();
+            oldChunkIndex += objects.Length;
         }
+        GameObject currentChunk = objects[oldChunkIndex];
+        Vector3 newPos = currentChunk.transform.position;
+        newPos.x += chunkSize * 5;
+        currentChunk.transform.position = newPos;
+        centerObject++;
+        centerObject = centerObject % objects.Length;
     }
 
     private void MoveLeft()
     {
-        foreach (Row row in rows)
-        {
-            row.MoveLeft();
-        }
-    }
-
-    private void MoveUp()
-    {
-        int oldChunkIndex = centerObject - 2;
-        if (oldChunkIndex < 0)
-        {
-            oldChunkIndex += rows.Length;
-        }
-        Transform currentChunk = rows[oldChunkIndex].transform;
-        Vector3 newPos = currentChunk.transform.position;
-        newPos.y += chunkSize * 5;
-        currentChunk.transform.position = newPos;
-        centerObject++;
-        centerObject = centerObject % rows.Length;
-    }
-
-    private void MoveDown()
-    {
         int oldChunkIndex = centerObject + 2;
-        oldChunkIndex = oldChunkIndex % rows.Length;
-        Transform currentChunk = rows[oldChunkIndex].transform;
+        oldChunkIndex = oldChunkIndex % objects.Length;
+        GameObject currentChunk = objects[oldChunkIndex];
         Vector3 newPos = currentChunk.transform.position;
-        newPos.y -= chunkSize * 5;
+        newPos.x -= chunkSize * 5;
         currentChunk.transform.position = newPos;
         centerObject--;
         if (centerObject < 0)
         {
-            centerObject += rows.Length;
+            centerObject += objects.Length;
         }
     }
 }
