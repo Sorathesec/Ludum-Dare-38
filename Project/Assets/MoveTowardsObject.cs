4af31da000baaa8c854a6c53bc94c6ad528e0a69
Quinn Nolan <quinn.nolan@students.plymouth.ac.uk>
2017-04-22 23:18:12 +0100

    Added zombie, and ability for it too run after player and attack them

---
+++ Project/Assets/MoveTowardsObject.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveTowardsObject : MonoBehaviour {
+
+    private Transform target;
+    public float speed = 5.0f;
+    private bool isMoving = false;
+    private Animator enemyAnimator;
+
+    void Start()
+    {
+        enemyAnimator = GetComponent<Animator>();
+    }
+
+
+    void FixedUpdate()
+    {
+        if (GameObject.FindWithTag("Player"))
+        {
+            target = GameObject.FindWithTag("Player").transform;
+        }
+
+        if (target != null)
+        {
+            if (GetComponent<Rigidbody2D>() != null)
+            {
+                GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+                GetComponent<Rigidbody2D>().angularVelocity = 0.0f;
+            }
+            transform.position = Vector3.MoveTowards(transform.position, target.position, speed * 0.01f);
+            if (enemyAnimator != null)
+            {
+                enemyAnimator.SetBool("isMoving", true);
+            }
+        }
+    }
+}
+
