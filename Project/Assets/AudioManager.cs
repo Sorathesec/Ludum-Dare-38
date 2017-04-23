8c7c0684cdaeab7f47fb90fb21cbe9e8dee617b7
Jacob Williams <squalljacob@hotmail.co.uk>
2017-04-23 08:40:19 +0100

    Added in an audio manager which stores a bunch of audio sources and plays to one of them a given clip

---
+++ Project/Assets/AudioManager.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Netaphous.Utilities;
+
+public class AudioManager : MonoBehaviour
+{
+    private static AudioManager instance;
+    [SerializeField]
+
+    protected int pooledAmount;
+    [SerializeField]
+    protected GameObject objectPrefab;
+
+    // Script logic
+    private AudioSource[] objectPool;
+    private bool[] activeObjects;
+
+    // Use this for initialization
+    void Start()
+    {
+        instance = this;
+
+        CreatePool();
+    }
+
+    /// <summary>
+    /// Initialises the object pool and fills it with game objects made from the prefab given
+    /// </summary>
+    protected void CreatePool()
+    {
+        objectPool = new AudioSource[pooledAmount];
+        activeObjects = new bool[pooledAmount];
+
+        GameObject temp;
+        for (int i = 0; i < pooledAmount; i++)
+        {
+            temp = Instantiate(objectPrefab) as GameObject;
+            temp.transform.parent = transform;
+            objectPool[i] = temp.GetComponent<AudioSource>();
+        }
+    }
+    /// <summary>
+    /// Returns a gameobject from the local pool if there is one available
+    /// </summary>
+    /// <returns> The gameobject from the pool </returns>
+    protected new AudioSource GetPooledItem()
+    {
+        for (int i = 0; i < pooledAmount; i++)
+        {
+            if (objectPool[i] != null && 
+                !objectPool[i].isPlaying)
+            {
+                return objectPool[i];
+            }
+        }
+        print("Fail");
+        return null;
+    }
+
+    public static void PlayAudioClip(AudioClip audio)
+    {
+        AudioSource source = instance.GetPooledItem();
+        
+        source.PlayOneShot(audio);
+    }
+}
