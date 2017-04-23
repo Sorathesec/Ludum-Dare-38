06d9b150bd282fa573813c63e193caef558d1675
Jacob Williams <squalljacob@hotmail.co.uk>
2017-04-23 12:35:24 +0100

    Increased the splitters damage

---
+++ Project/Assets/Scripts/Enemies/SplitterZombie.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SplitterZombie : ZombieHealth
+{
+    [SerializeField]
+    private GameObject splitPrefab;
+    [SerializeField]
+    private int numOfSplits;
+    [SerializeField]
+    private Vector2 spawnRange;
+
+    private GameObject[] splits;
+
+    void Start()
+    {
+        splits = new GameObject[numOfSplits];
+        for(int i = 0; i < numOfSplits; i++)
+        {
+            splits[i] = Instantiate(splitPrefab);
+            splits[i].SetActive(false);
+        }
+    }
+
+    protected override void Die()
+    {
+        if (deathClip != null &&
+            enemyAudio != null)
+        {
+            enemyAudio.PlayOneShot(deathClip);
+        }
+
+        renderer.enabled = false;
+        attack.enabled = false;
+        collider.enabled = false;
+        trigger.enabled = false;
+
+        foreach(GameObject obj in splits)
+        {
+            float rndX = Random.Range(-spawnRange.x, spawnRange.x);
+            float rndY = Random.Range(-spawnRange.y, spawnRange.y);
+            obj.transform.position = new Vector2(transform.position.x + rndX, transform.position.y + rndY);
+            obj.SetActive(true);
+        }
+    }
+}
