c0b919d094aa1dd2aaaeffbe43d4fd1103c7381b
Jacob Williams <squalljacob@hotmail.co.uk>
2017-04-23 09:01:53 +0100

    Changed the bullet scripts to inherit from a base and then add to it

---
+++ Project/Assets/Scripts/Player/Weapons/RegularBullet.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RegularBullet : Bullet2D
+{
+    protected override void CollisionAction(Collider2D other)
+    {
+        other.SendMessage("TakeDamage", damage);
+
+        health--;
+        if (health > 0)
+        {
+            return;
+        }
+        gameObject.SetActive(false);
+    }
+}
