d0d2cb4f968a1de73a606d2753edb8f44e1c2dfb
Jacob Williams <squalljacob@hotmail.co.uk>
2017-04-23 08:06:05 +0100

    Added sound to the zombies, choosing between a list of possible sounds at startup to use for death and damaging and fixed a bug with the weapons as three used the same prefab

---
+++ Project/Assets/ChooseRandomAudio.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChooseRandomAudio : MonoBehaviour
+{
+    [SerializeField]
+    private AudioClip[] damageClips;
+    [SerializeField]
+    private AudioClip[] deathClips;
+    // Use this for initialization
+    void Start ()
+    {
+        IsShootable shootable = GetComponent<IsShootable>();
+        int rnd = Random.Range(0, damageClips.Length);
+        shootable.damageClip = damageClips[rnd];
+        shootable.deathClip = deathClips[rnd];
+	}
+}
--- Project/Assets/IsShootable.cs
+++ Project/Assets/IsShootable.cs
@@ -6,36 +6,60 @@ public class IsShootable : MonoBehaviour
 {
     public int health = 10;
     public AudioClip deathClip;
+    public AudioClip damageClip;
     //public GameObject explosionPrefab;
     //public float adjustExplosionAngle = 0.0f;
 
     AudioSource enemyAudio;
+    SpriteRenderer renderer;
+    ZombieAttack attack;
+    CircleCollider2D collider;
 
     public void Awake()
     {
         enemyAudio = GetComponent<AudioSource>();
+        renderer = GetComponent<SpriteRenderer>();
+        attack = GetComponent<ZombieAttack>();
+        collider = GetComponent<CircleCollider2D>();
+    }
+
+    void OnEnable()
+    {
+        renderer.enabled = true;
+        attack.enabled = true;
+        collider.enabled = true;
     }
 
     public void TakeDamage(int damage)
     {
         health -= damage;
-
-        if (enemyAudio != null)
-        {
-            enemyAudio.Play();
-        }
+        
         if (health <= 0)
         {
-            if (deathClip != null)
+            if (deathClip != null &&
+                enemyAudio != null)
             {
-                enemyAudio.clip = deathClip;
-            }
-            if (enemyAudio != null)
-            {
-                enemyAudio.Play();
+                enemyAudio.PlayOneShot(deathClip);
             }
 
-            gameObject.SetActive(false);
+            renderer.enabled = false;
+            attack.enabled = false;
+            collider.enabled = false;
+
+            Invoke("Disable", 0.5f);
         }
+        else
+        {
+            if (damageClip != null &&
+                enemyAudio != null)
+            {
+                enemyAudio.PlayOneShot(damageClip);
+            }
+        }
+    }
+
+    private void Disable()
+    {
+        gameObject.SetActive(false);
     }
 }
