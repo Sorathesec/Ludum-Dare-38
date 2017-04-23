b4d2fb13763dc351b97f76ef191994f896436721
Jacob Williams <squalljacob@hotmail.co.uk>
2017-04-23 04:09:07 +0100

    Converted the BasicWeapon to be a framework for other weapon scripts, created a spread weapon script from this and a shotgun

---
+++ Project/Assets/SpreadWeapon.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpreadWeapon : BasicWeapon
+{
+    [SerializeField]
+    private int numOfBullets = 3;
+    [SerializeField]
+    private Transform[] bulletSpawns;
+    
+    protected override void Fire()
+    {
+        canFire = false;
+
+        for(int i = 0; i < numOfBullets; i++)
+        {
+            FireSingleBullet(bulletSpawns[i]);
+
+            if(bulletsInClip <= 0)
+            {
+                break;
+            }
+        }
+
+        Invoke("ReadyToFire", fireRate);
+    }
+}
